9d062ce061637dc72b1ccd231ac8c62c32f2b41e
srsajjad960 <srsajjad960@gmail.com>
2022-01-06 14:26:18 +0600

    all files update

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 
+//  Age Calculator
     const button = document.querySelector('#ageCheck');
     const name = document.querySelector('#name');
     const year = document.querySelector('#birthYear');
@@ -6,7 +7,11 @@
     
 
     button.addEventListener('click', () => {
+
+        name.style.border = ( name.value == '' ) ? "1px solid red" : "1px solid grey";
         
+        year.style.border = ( year.value == '' ) ? "1px solid red" : "1px solid grey";
+
         if( name.value == '' || year.value == '' ){
             result.innerHTML = `<p class="alert alert-danger"> All fields are Required </p>`;
         }else{
@@ -19,4 +24,92 @@
 
     });
 
-    
+
+//  Registration Form
+    const mainForm = document.querySelector('#mainForm');
+    const firstName = document.querySelector('#firstName');
+    const username = document.querySelector('#username');
+    const email = document.querySelector('#email');
+    const password = document.querySelector('#password');
+    const signUp = document.querySelector('#signUp');
+    const showResult = document.querySelector('.showResult');
+    const userShow = document.querySelector('#userShow');
+
+    mainForm.addEventListener('submit', function(e){
+        e.preventDefault();
+
+        firstName.style.border = ( firstName.value == '' ) ? "1px solid red" : "1px solid grey";
+
+        username.style.border = ( username.value == '' ) ? "1px solid red" : "1px solid grey";
+
+        email.style.border = ( email.value == '' ) ? "1px solid red" : "1px solid grey";
+
+        password.style.border = ( password.value == '' ) ? "1px solid red" : "1px solid grey";
+        
+        if( firstName.value == '' || username.value == '' || email.value == '' || password.value == '' ){
+            showResult.innerHTML = `<p class="alert alert-danger"> All fields are required </p>`;
+        }else{
+            showResult.innerHTML = `<p class="alert alert-success"> Data Stable </p>`;
+        }
+
+        firstName.value = '';
+        username.value = '';
+        email.value = '';
+        password.value = '';
+
+    });
+
+
+//  Currency Converter
+    const currencyForm = document.querySelector('#currencyForm');
+    const currencyEmail = document.querySelector('#currencyEmail');
+    const amount = document.querySelector('#amount');
+    const select = document.querySelector('#select');
+    const messages = document.querySelector('.messages');
+
+    currencyForm.onsubmit = (e) => {
+        e.preventDefault();
+
+        currencyEmail.style.border = ( currencyEmail.value == '' ) ? "1px solid red" : "1px solid grey";
+
+        amount.style.border = ( amount.value == '' ) ? "1px solid red" : "1px solid grey";
+
+        select.style.border = ( select.value == '' ) ? "1px solid red" : "1px solid grey";
+
+        if( currencyEmail.value == '' || amount.value == '' || select.value == '' ){
+            messages.innerHTML = `<p class="alert alert-danger"> All fields are Required </p>`;
+        }else{
+            messages.innerHTML = myCurrency(amount.value, select.value );
+
+            currencyEmail.value = '';
+            amount.value = '';
+            select.value = '';
+        }   
+        
+    };
+
+
+//  Marriage Age Check Calculator
+    const mAgeform = document.querySelector('#mAgeform');
+    const marriageName = document.querySelector('#marriageName');
+    const marriageAge = document.querySelector('#marriageAge');
+    const marriageButton = document.querySelector('#marriageButton');
+    const ageResult = document.querySelector('.ageResult');
+
+    mAgeform.onsubmit = (e) => {
+        e.preventDefault();
+
+        marriageName.style.border = ( marriageName.value == '' ) ? "1px solid red" : "1px solid grey";
+
+        marriageAge.style.border = ( marriageAge.value == '' ) ? "1px solid red" : "1px solid grey";
+
+        if( marriageName.value == '' || marriageAge.value == '' ){
+            ageResult.innerHTML = `<p class="alert alert-danger"> All fields are required </p>`;
+        }else{
+            ageResult.innerHTML = mageCheck(marriageName.value, marriageAge.value);
+
+            marriageName.value = '';
+            marriageAge.value = '';
+        }
+        
+    };
--- function.js
+++ function.js
@@ -1,12 +1,12 @@
 /**
- * Age Calculator
+ * Age Calculator Function
  * @param {*} name 
  * @param {*} year 
  * @returns 
  */
     function ageCal(name, year){
         let date = new Date();
-        age = date.getFullYear() - year;
+        let age = date.getFullYear() - year;
 
         let ageStatus = checkAgeStatus(age);
         return` <p class="alert alert-${ ageStatus.status }"> Hi ${name} You are ${age} years old & you are ${ ageStatus.name }</p>`
@@ -14,7 +14,7 @@
 
 
 /**
- * Check Age Status
+ * Check Age Status Function
  * @param {*} age 
  * @returns 
  */
@@ -51,3 +51,52 @@
             };
         }
     };
+
+
+
+/**
+ * Currency Converter function
+ * @param {*} amount 
+ * @param {*} currency 
+ * @returns 
+ */
+    function myCurrency( amount, currency ){
+        if(amount == '' || currency == ''){
+            return ` All fields are required `;
+        }else if( currency == 'USD' ){
+            return ` ${amount} ${currency} = ${amount * 84} taka`;
+        }else if( currency == 'CAD' ){
+            return ` ${amount} ${currency} = ${amount * 68} taka`;
+        }else if( currency == 'POUND' ){
+            return ` ${amount} ${currency} = ${amount * 116} taka`;
+        }else if( currency == 'EURO' ){
+            return ` ${amount} ${currency} = ${amount * 98} taka`;
+        }else{
+            return`This Currency is't Our System`;
+        }
+    }
+
+
+/**
+ * Marriage Age Check Function
+ * @param {*} name 
+ * @param {*} age 
+ * @returns 
+ */
+    function mageCheck(name, age){
+        if( age > 0 && age <= 10 ){
+            return`Hi ${ name } Your ${ age } years old. "You baby" Pls wait for marriage ${ 21 - age } years.`;
+        }else if( age >= 11 && age < 21 ){
+            return`Hi ${ name } Your ${ age } years old. "You young" pls wait for marriage ${ 21 - age } years.`;
+        }else if( age >= 21 && age < 22 ){
+            return`Hi ${ name } Your ${ age } years old. "You adult" your age perfect for marriage ${ 21 - age } years.`;
+        }else if( age >= 22 && age < 45 ){
+            return`Hi ${ name } Your ${ age } years old. Apnar ${ age - 21 } bosor age biyer boyosh hoyese`;
+        }else if( age >= 45 && age < 65 ){
+            return`Hi ${ name } Your ${ age } years old. Apni boyoshko &  ${ age - 21 } bosor age biye kora uchit silo apnar.`;
+        }else if( age >= 65 && age <= 200 ){
+            return`Hi ${ name } Your ${ age } years old. You extra old & rest a thaka uchit Akhon biye kora thik hobe na.`;
+        }
+   }
+
+   
